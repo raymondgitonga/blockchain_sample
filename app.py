e055cfab6306d25234eba282fd4daa73d8a4380e
raymond gitonga <raytosh95@gmail.com>
2020-07-20 04:37:56 +0300

    Init genesis block

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ import hashlib
 import _json
 from flask import Flask
 
-
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.creat_block(proof = 1, previous_hash = '0')
 
