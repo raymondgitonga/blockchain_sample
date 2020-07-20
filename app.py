b4a43004cf81c8a99886b66f5f995a73386b5672
raymond gitonga <raytosh95@gmail.com>
2020-07-20 04:56:16 +0300

    Add Genesis block

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import datetime
 import hashlib
-import _json
+import json
 from flask import Flask
+
 
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.creat_block(proof = 1, previous_hash = '0')
+        self.creat_block(proof=1, previous_hash='0')
 
+    # genesis block
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
